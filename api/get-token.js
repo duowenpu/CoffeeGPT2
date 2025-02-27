8d64df604f004e19803de7c603ee7f7d1da54dcf
duowenpu <56353993+duowenpu@users.noreply.github.com>
2025-02-27 10:35:56 -0800

    Delete api directory

--- api/get-token.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    // 从环境变量中获取 Token
-    const token = process.env.COZE_TOKEN;
-    if (!token) {
-        return res.status(500).json({ error: 'Server configuration error' });
-    }
-    
-    res.status(200).json({ token });
-}
